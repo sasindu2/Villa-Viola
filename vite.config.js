49762a0194342256e5885d2f79522e49b16ce007
sasindu2 <pujana.sasindu@gmail.com>
2025-08-04 00:19:17 +0530

    Fix dist directory error: Add framework null and explicit build config

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,8 @@ export default defineConfig({
         },
     },
     build: {
+        outDir: 'public/build',
+        manifest: true,
         rollupOptions: {
             output: {
                 manualChunks: {
